dfefcaedea8592388b65afdd8782fdb208782ed6
sabyasachi135 <sabyasachi2189@gmail.com>
2014-11-18 17:20:39 +0530

    Deleted

--- TFSAutomation/src/main/java/com/taxiforsure/driver/MyDriver.java
+++ TFSAutomation/src/main/java/com/taxiforsure/driver/MyDriver.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -68,8 +69,13 @@ public class MyDriver {
 	}
 	
 	public static DesiredCapabilities getChromeProfile(){
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--test-type");
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		capability.setCapability(ChromeOptions.CAPABILITY, "--test-type");
+		
+		
 		
 		return capability;
 		
@@ -94,6 +100,7 @@ public class MyDriver {
 			String chromeDriverPath = relativePath + "/lib/chromedriver.exe";
 			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 			ESS_LOGS.debug("Opening Chrome Browser");
+			
 			driver = new ChromeDriver(MyDriver.getChromeProfile());
 		}
 
